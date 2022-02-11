174f2823ebc0b0242a6aaf6cce6a6e65fbc1eff5
Naveen Sai <38736123+naveen675@users.noreply.github.com>
2022-02-11 12:22:32 +0530

    Min No Of Del and Ins to convert String A to B

---
+++ DP/Tabulation/MinimumNoOfDelAndIns.java
@@ -0,0 +1,40 @@
+class Solution{
+  
+  static int longestCommonSubSequence(String s1,String s2,int n,int m,int [][] dp) {
+		
+		for(int i=0;i<=n;i++) {
+			for(int j =0;j<=m;j++) {
+				
+				if(i == 0 || j==0) {
+					dp[i][j] = 0;
+				}
+				
+				else {
+					if(s1.charAt(i-1) == s2.charAt(j-1)) {
+						dp[i][j] = dp[i-1][j-1]+1;
+					}
+					else {
+						dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+					}
+				}
+			}
+		}
+		return dp[n][m];
+	}
+  
+  static int minimumDelAndInser(String s1,String s2) {
+		
+		int n = s1.length();
+		int m = s2.length();
+		int [][] dp = new int[n+1][m+1];
+		
+		int lcs = longestCommonSubSequence(s1,s2,n,m,dp);
+		
+		
+		
+		int noOfdel = n - lcs;
+		int noOfIns = m - lcs;
+		return (noOfdel+noOfIns);
+	}
+  
+}
