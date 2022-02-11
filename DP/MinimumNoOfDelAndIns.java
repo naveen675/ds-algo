b6bde44cfd84b646e1cae728987a018c62d9f384
Naveen Sai <38736123+naveen675@users.noreply.github.com>
2022-02-11 12:08:35 +0530

    Min No Of del and Ins to change String A to B

---
+++ DP/MinimumNoOfDelAndIns.java
@@ -0,0 +1,32 @@
+class Solution{
+  
+  static int longestCommonSubSequence(String s1,String s2,int n,int m) {
+		
+		if(n == 0 || m == 0) {
+			return 0;
+		}
+		
+		if(s1.charAt(n-1) == s2.charAt(m-1)) {
+			
+			return 1+longestCommonSubSequence(s1,s2,n-1,m-1);
+		}
+		else {
+			
+			return Math.max(longestCommonSubSequence(s1,s2,n-1,m), longestCommonSubSequence(s1,s2,n,m-1));
+			
+		}
+	}
+  
+  static int minimumDelAndInser(String s1,String s2) {
+		
+		int n = s1.length();
+		int m = s2.length();
+		
+		int lcs = longestCommonSubSequence(s1,s2,n,m);
+		
+		int noOfdel = n - lcs;
+		int noOfIns = m - lcs;
+		return (noOfdel+noOfIns);
+	}
+  
+}
