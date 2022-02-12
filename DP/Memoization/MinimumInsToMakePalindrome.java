269f72b3df010159e900563079de6e898d5973e1
Naveen Sai <38736123+naveen675@users.noreply.github.com>
2022-02-12 20:19:51 +0530

    Min insertions to Make Palindrome

---
+++ DP/Memoization/MinimumInsToMakePalindrome.java
@@ -0,0 +1,50 @@
+class Solution{
+  
+  static int longestPalindromicSubSeq(String s,int start,int end,int [][] dp) {
+		
+		if(dp[start][end] != -1) {
+		
+			return dp[start][end];
+		}
+		
+		
+		if(start == end) {
+			
+			dp[start][end] = 1;
+			return dp[start][end];
+		}
+		if(start > end) {
+			
+			dp[start][end] =0;
+			return dp[start][end];
+		}
+		
+		if(s.charAt(start) == s.charAt(end)) {
+			
+			dp[start][end] =  2+longestPalindromicSubSeq(s,start+1,end-1,dp);
+		}
+		else {
+			
+			dp[start][end] =  Math.max(longestPalindromicSubSeq(s,start+1,end,dp), longestPalindromicSubSeq(s,start,end-1,dp));
+		}
+		
+		return dp[start][end];
+		
+	}
+  
+  static int minimumInsToMakePalindrome(String s) {
+		
+		int end = s.length();
+		int start = 0;
+		int [][] dp = new int[end+1][end+1];
+		for(int i=0;i<=end;i++) {
+			for(int j =0;j<=end;j++) {
+				
+				Arrays.fill(dp[i], -1);
+			}
+		}
+		int lps = longestPalindromicSubSeq(s,start,end-1,dp);
+		return end-lps;
+	}
+	
+}
