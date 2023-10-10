601044746728cb80f807ba1261464593a27412d8
Naveen Sai <38736123+naveen675@users.noreply.github.com>
2023-10-10 17:36:59 +0530

    Another Solution Added

--- Arrays/Searching/Negative_Num_in_Sorted_Array.java
+++ Arrays/Searching/Negative_Num_in_Sorted_Array.java
@@ -1,4 +1,52 @@
 class Solution {
+
+
+//SOLUTION 1
+
+	public static int negativeElementLastIndex(int [] arr, int low,int high){
+
+		
+		if(low > high){
+	
+			return -1;
+	}
+		
+		int mid = (low+high)/2;
+		
+		
+	
+		if(arr[mid] < 0){
+		
+			if(mid == arr.length-1 || arr[mid+1] >= 0){
+			
+				return mid;
+	}
+			else if(arr[mid+1] < 0){
+	
+				return negativeElementLastIndex(arr,mid+1,high);
+	}
+	}
+		
+		return negativeElementLastIndex(arr,low,mid-1);
+	}
+	
+	
+	public static int negativeElementRange(int [] arr, int low,int high){
+		
+		int lastIndex = negativeElementLastIndex(arr,low,high);
+		int totalNegativeElements = lastIndex+1;
+		return totalNegativeElements;
+	}
+	
+		
+
+
+
+
+	
+ -------------------------------------------------------------------------------------------------------------------------	
+	
+//SOLUTION 2	
 	
 	int binarySearch(int [] arr, int low, int high){
 		
