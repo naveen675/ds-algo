5aacd565fef8f2707a0b3f0cad791000c07f06f0
naveen675 <spnaveen675@gmail.com>
2023-10-11 15:16:24 +0530

    Added solition2 for insert position

--- Arrays/Searching/Insert_position_of_element_in_sorted_Array.java
+++ Arrays/Searching/Insert_position_of_element_in_sorted_Array.java
@@ -1,48 +1,95 @@
+package Searching;
+
 class Solution {
 	
-	int binarySearch(int [] arr,int key, int low, int high){
-		
-		
-		if(low >= high){
-			
-			
-			
-			if(arr[low] > key){
-				
-				return low;
-			}
-			else{
-				return low+1;
-			}
-		}
-		
+//
+//	// Solution 1
+//	int binarySearch(int [] arr,int key, int low, int high){
+//
+//
+//		if(low >= high){
+//
+//
+//
+//			if(arr[low] > key){
+//
+//				return low;
+//			}
+//			else{
+//				return low+1;
+//			}
+//		}
+//
+//		int mid = (low+high)/2;
+//
+//		if(arr[mid] == key){
+//
+//			return mid;
+//		}
+//
+//		else if(key > arr[mid]){
+//
+//			return binarySearch(arr,key, mid+1, high);
+//
+//		}
+//
+//		else{
+//			return binarySearch(arr, key, low, mid-1);
+//		}
+//	}
+//
+//	int getInsertPosition (int[] arr, int key) {
+//		// add your logic here
+//
+//		int low =0;
+//		int high = arr.length-1;
+//		int keyIndex = binarySearch(arr,key,low,high);
+//
+//		return keyIndex;
+//
+//
+//	}
+
+	// Solution 2
+
+	public static int searchUtil(int [] arr, int low, int high, int target){
+
+		if(low > high){ return low;}
+
 		int mid = (low+high)/2;
-		
-		if(arr[mid] == key){
-			
-			return mid;
-		}
-		
-		else if(key > arr[mid]){
-			
-			return binarySearch(arr,key, mid+1, high);
-			
-		}
-		
-		else{
-			return binarySearch(arr, key, low, mid-1);
-		}
+
+		if(arr[mid] == target && arr[mid-1] != target){ return mid;}
+
+		else if(arr[mid] < target) {return searchUtil(arr,mid+1, high,target); }
+
+		return searchUtil(arr,low, mid-1, target);
+
+
 	}
-	
-	int getInsertPosition (int[] arr, int key) {
-		// add your logic here
-		
-		int low =0;
+
+	public static int search(int [] arr,int target){
+
+		int low=0;
+
 		int high = arr.length-1;
-		int keyIndex = binarySearch(arr,key,low,high);
-		
-		return keyIndex;
-		
-		
+
+		if(arr[low] >= target){ return low; }
+
+		else if(arr[high] < target) { return high+1; }
+
+		return searchUtil(arr,low,high,target);
 	}
+
+	public static void main(String [] args){
+
+		int [] arr = {1,2,3,4,5,6,7,8};
+
+		int target = 7;
+
+		int index = search(arr, target);
+
+		System.out.println(index);
+
+	}
+
 }
